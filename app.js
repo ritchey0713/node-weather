dc21a8f698848fbe68ac19aea82054584462dd9b
Dj <ritchey0713@gmail.com>
2020-01-28 14:47:27 -0500

    require geocode

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const request = require("request")
 
 const API = require("./APISecrets")
 
+const geoCode = require("./utils/geocode")
+
 
 // const weatherUrl = "https://api.darksky.net/forecast/9c0eb2fc8b0d0c20e73315c147dc71f8/37.8267,-122.4233"
 
